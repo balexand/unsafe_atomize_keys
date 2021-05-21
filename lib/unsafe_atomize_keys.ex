340b9aa01fa88d3e96d7c51ebcc1dd5d1e2d555f
Brian Alexander <balexand@gmail.com>
2021-05-21 11:07:50 -0600

    initial commit

---
+++ lib/unsafe_atomize_keys.ex
@@ -0,0 +1,18 @@
+defmodule UnsafeAtomizeKeys do
+  @moduledoc """
+  Documentation for `UnsafeAtomizeKeys`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UnsafeAtomizeKeys.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/unsafe_atomize_keys_test.exs
@@ -0,0 +1,8 @@
+defmodule UnsafeAtomizeKeysTest do
+  use ExUnit.Case
+  doctest UnsafeAtomizeKeys
+
+  test "greets the world" do
+    assert UnsafeAtomizeKeys.hello() == :world
+  end
+end
