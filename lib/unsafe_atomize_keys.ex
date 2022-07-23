2225a8bed028f932fa79d875f5d12a7ea926f7a2
Brian Alexander <balexand@gmail.com>
2022-07-23 12:45:01 -0600

    optional key transform function

--- lib/unsafe_atomize_keys.ex
+++ lib/unsafe_atomize_keys.ex
@@ -4,6 +4,9 @@ defmodule UnsafeAtomizeKeys do
   unchanged. The conversion will deeply traverse nested maps and lists. See README for explanation
   of why this function is "unsafe".
 
+  The optional `func` argument is a function that accepts a string and performs some kind of
+  transformation on that string.
+
   ## Examples
 
     iex> unsafe_atomize_keys(%{"a" => "b", :c => "d"})
@@ -14,26 +17,29 @@ defmodule UnsafeAtomizeKeys do
 
     iex> unsafe_atomize_keys(%{"list" => [%{"key" => "value"}]})
     %{list: [%{key: "value"}]}
+
+    iex> unsafe_atomize_keys(%{"theKey" => "b", :c => "d"}, &Macro.underscore/1)
+    %{the_key: "b", c: "d"}
   """
-  def unsafe_atomize_keys(%{} = map) do
-    deep_atomize(map)
+  def unsafe_atomize_keys(%{} = map, func \\ &Function.identity/1) when is_function(func) do
+    deep_atomize(map, func)
   end
 
-  defp deep_atomize(%{} = map) do
+  defp deep_atomize(%{} = map, func) do
     map
     |> Enum.map(fn
       {k, v} when is_binary(k) ->
-        {String.to_atom(k), deep_atomize(v)}
+        {k |> func.() |> String.to_atom(), deep_atomize(v, func)}
 
       {k, v} ->
-        {k, deep_atomize(v)}
+        {k, deep_atomize(v, func)}
     end)
     |> Map.new()
   end
 
-  defp deep_atomize(list) when is_list(list) do
-    Enum.map(list, &deep_atomize/1)
+  defp deep_atomize(list, func) when is_list(list) do
+    Enum.map(list, &deep_atomize(&1, func))
   end
 
-  defp deep_atomize(v), do: v
+  defp deep_atomize(v, _func), do: v
 end
